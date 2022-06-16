7e13f5d02953971c7d6ab937192146be532afcac
PirateNahid <piratenahid46@gmail.com>
2022-06-16 23:49:50 +0600

    v

--- js/script.js
+++ js/script.js
@@ -43,23 +43,16 @@ Answer[18] = ["Umm.. Well, I was named after Juno. Juno was an ancient Roman god
 
 
 function Time() {
-    // Creating object of the Date class
     var date = new Date();
-    // Get current hour
     var hour = date.getHours();
-    // Get current minute
     var minute = date.getMinutes();
-    // Get current second
     var second = date.getSeconds();
-    // Variable to store AM / PM
     var period = "";
-    // Assigning AM / PM according to the current hour
     if (hour >= 12) {
     period = "PM";
     } else {
     period = "AM";
     }
-    // Converting the hour in 12-hour format
     if (hour == 0) {
     hour = 12;
     } else {
@@ -67,18 +60,13 @@ function Time() {
     hour = hour - 12;
     }
     }
-    // Updating hour, minute, and second
-    // if they are less than 10
     hour = update(hour);
     minute = update(minute);
     second = update(second);
-    // Adding time elements to the div
     document.getElementById("digital-clock").innerText = hour + " : " + minute + " : " + second + " " + period;
-    // Set Timer to 1 sec (1000 ms)
     setTimeout(Time, 1000);
    }
-    // Function to update time elements if they are less than 10
-    // Append 0 before time elements if they are less than 10
+    
    function update(t) {
     if (t < 10) {
     return "0" + t;
